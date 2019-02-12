5eb1292f2b31245bd43e0528f91c64a85355afcc
Lucas Oliveira (BFSI - Brazil) <LU20066815@wipro.com>
2019-02-12 11:09:38 -0200

    secondDay

---
+++ projects/blockchain/src/lib/models/block-data.ts
@@ -0,0 +1,13 @@
+import { Block } from './block';
+import { Transaction } from './transaction';
+
+
+export class BlockData {
+    public transactions: Transaction[];
+    public index: number;
+
+    constructor(block: Block){
+        this.transactions = block.transactions;
+        this.index = block.index;
+    }
+}
--- projects/blockchain/src/lib/models/blockchain.ts
+++ projects/blockchain/src/lib/models/blockchain.ts
@@ -1,11 +1,64 @@
 import { Block } from './block';
 import { Transaction } from './transaction';
+import { v4 as uuid } from 'uuid';
+import { sha256 } from 'hash.js';
+import { BlockData } from './block-data';
+
 
 export class Blockchain {
-    public chain: Block[]; 
+    public chain: Block[];
     public pendingTransactions: Transaction[] = [];
-    public noUrl: string;
+    public nodeUrl: string;
     public networdNodes: string[] = [];
+    private dificulty: number;
 
-    
+    constructor(GENESIS_BLOCK: Block, dificulty = 2) {
+        this.chain = [GENESIS_BLOCK];
+        this.nodeUrl = uuid();
+        this.dificulty = dificulty;
+    }
+
+    newBlock(nonce: string | number, previousHash: string, hash: string): Block {
+        const newBlock = new Block(
+            this.chain.length + 1,
+            Date.now(),
+            this.pendingTransactions,
+            nonce.toString(),
+            hash,
+            previousHash,
+        );
+        this.pendingTransactions = [];
+        this.chain.push(newBlock);
+        return newBlock;
+    }
+
+    getLatestBlock(): Block {
+        return this.chain[this.chain.length - 1];
+    }
+
+    hashBlock(
+        previousHash: string,
+        blockData: BlockData,
+        nonce: string | number
+    ): string {
+        const data = previousHash + JSON.stringify(blockData) + nonce.toString();
+
+        const hash = sha256().update(data).digest('hex');
+        return hash;
+    }
+
+    proofOfWorkNonce(
+        previousHash: string,
+        currentBlockData: BlockData
+    ): string {
+        let nonce = 0;
+        let hash = this.hashBlock(previousHash, currentBlockData, nonce);
+        
+        while (hash.substring(0, this.dificulty) !== this.chain[0].hash.substring(0, this.dificulty)) {
+            nonce++;
+            hash = this.hashBlock(previousHash, currentBlockData, nonce);
+        }
+        return nonce.toString();
+    }
+
 }
